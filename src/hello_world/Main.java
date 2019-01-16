c20d63c06a028d5bbeceffca4b565d45d61b4b1f
takuhase <hasetakunoid3228@icloud.com>
2019-01-17 05:34:17 +0900

    initial commit

---
+++ src/hello_world/Main.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package hello_world;
+
+/**
+ * @author Owner
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello, World!!!");
+	}
+}
